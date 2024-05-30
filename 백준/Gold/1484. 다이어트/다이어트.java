29b5392d8cf5c6ec20d32a10dc9a9d507aaa6a97
eejuuung <46306166+eejuuung@users.noreply.github.com>
2024-05-30 16:01:07 +0900

    [Gold V] Title: 다이어트, Time: 88 ms, Memory: 11884 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -11,26 +8,50 @@ public class Main {
 
         long G = Long.parseLong(br.readLine());
 
-        for(long i=1, j=0;i<=100000;i++,j++){
-            if(((i*i) - (j*j))> G)
+        long left = 1;
+        long right = 2;
+
+        while (right<=100000) {
+
+            if(((right*right) - ((right-1)*(right-1)))> G)
                 break;
 
-            for(long n = 1;n <i;n++){
-
-                if((i*i) - (n*n) == G){
-                    isin = true;
-                    bw.write(String.valueOf(i)+"\n");
-                    break;
-                }
+            if((right * right) - (left * left) < G){
+                right++;
+                continue;
+            }
+            else if ((right * right) - (left * left) > G){
+                left++;
+                continue;
+            }
+            else if ((right * right) - (left * left) == G){
+                isin = true;
+                bw.write(String.valueOf(right)+"\n");
+                right++;
+                continue;
             }
         }
+
+
+//        for(long i=1, j=0;i<=100000;i++,j++){
+//            if(((i*i) - (j*j))> G)
+//                break;
+//
+//            for(long n = 1;n <i;n++){
+//
+//                if((i*i) - (n*n) == G){
+//                    isin = true;
+//                    bw.write(String.valueOf(i)+"\n");
+//                    break;
+//                }
+//            }
+//        }
+
         if(isin){
             bw.flush();
         }else{
             bw.write("-1\n");
             bw.flush();
         }
-
-
     }
 }
