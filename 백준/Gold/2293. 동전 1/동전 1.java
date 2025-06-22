01298579b086e11a0509fe6f8f6a461f1e5211f0
JiYoung <46306166+eejuuung@users.noreply.github.com>
2025-06-22 23:23:57 +0900

    [Gold IV] Title: 동전 1, Time: 72 ms, Memory: 11752 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(stz.nextToken());
+        int K = Integer.parseInt(stz.nextToken());
+        int dp[] = new int[K + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            for (int j = num; j <= K; j++) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+}
