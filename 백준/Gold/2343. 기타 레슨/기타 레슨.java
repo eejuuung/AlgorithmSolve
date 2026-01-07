d0035bcf26d0b4c389644d5d8d13694f7573c39a
JiYoung <46306166+eejuuung@users.noreply.github.com>
2026-01-07 19:36:02 +0900

    [Gold V] Title: 기타 레슨, Time: 240 ms, Memory: 22520 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.java
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -6,16 +6,14 @@ public class Main {
     static int[] arr;
 
     static boolean isoKay(int X) {
-        int total = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = i; j <= N; j++) {
-                if (arr[j] - arr[i - 1] > X) {
-                    i = j - 1;
-                    total++;
-                    break;
-                }
+        int total = 0, cnt = 1;
+        for (int i = 0; i < N; i++) {
+            if (total + arr[i] > X) {
+                cnt++;
+                total = 0;
             }
-            if (total + 1 > M)
+            total += arr[i];
+            if (cnt > M)
                 return false;
         }
         return true;
@@ -26,21 +24,19 @@ public class Main {
         StringTokenizer stz = new StringTokenizer(br.readLine());
         N = Integer.parseInt(stz.nextToken());
         M = Integer.parseInt(stz.nextToken());
-        arr = new int[N + 1];
-        int minX = 0, maxX = 0;
+        arr = new int[N];
 
         stz = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            int num = Integer.parseInt(stz.nextToken());
+        int left = 0, right = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(stz.nextToken());
 
-            arr[i] = arr[i - 1] + num;
-            minX = Integer.max(minX, num);
+            left = Integer.max(left, arr[i]);
+            right += arr[i];
         }
-        maxX = arr[N];
 
-        int left = minX, right = arr[N], mid;
         while (left < right) {
-            mid = (left + right) / 2;
+            int mid = (left + right) / 2;
 
             if (isoKay(mid)) {
                 right = mid;
