288b609325fe2f6a1869dbc0ce7f76840aa0d1d7
JiYoung <46306166+eejuuung@users.noreply.github.com>
2026-05-01 16:20:20 +0900

    [level 2] Title: 힌트 스테이지, Time: 5.25 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -1,64 +1,51 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
 
     static int[][] cost;
     static int[][] hint;
-    
-    static int answer;
-    
-    static HashMap<String, Integer> memo;
 
-    public static void dfs(int stage, int totalCost, int bundle) {
+    static int answer;
+
+    public static void dfs(int stage, int totalCost, int[] hintNum) {
+        if (totalCost >= answer)
+            return;
+
         if (stage == cost.length) {
             answer = Math.min(answer, totalCost);
             return;
         }
-
-        if (totalCost >= answer)
-            return;
         
-        String key = stage + "," + bundle;
-        if(memo.containsKey(key) && memo.get(key)<=totalCost)
-            return;
-        memo.put(key,totalCost);
+        int use = Math.min(hintNum[stage], cost[stage].length - 1);
+        int nextCost = totalCost + cost[stage][use];
 
-        // 구매한 번들을 기준으로 현재stage의 힌트갯수 체크
-        int maxUse = 0;
-        for(int i = 0; i < hint.length; i++){
-            if((bundle & (1<<i)) == 0)
-                continue;
-            
-            for(int j=1; j < hint[i].length; j++){
-                if(hint[i][j] == stage+1)
-                    maxUse++;
+        // 번들 구매 X
+        dfs(stage + 1, nextCost, hintNum);
+
+        // 번들 구매 O
+        if (stage < hint.length) {
+            for (int k = 1; k < hint[stage].length; k++) {
+                hintNum[hint[stage][k] - 1]++;
             }
-        }
-        maxUse = Math.min(maxUse, cost[stage].length-1);
-        
-        for (int use = 0; use <= maxUse; use++) {
-            int nextCost = totalCost + cost[stage][use];
-            
-            // 번들 구매 X
-            dfs(stage + 1, nextCost, bundle);
 
-            // 번들 구매 O
-            if(stage < hint.length){
-                dfs(stage + 1, nextCost + hint[stage][0], bundle | (1<<stage));
+            dfs(stage + 1,
+                nextCost + hint[stage][0],
+                hintNum);
+
+            for (int k = 1; k < hint[stage].length; k++) {
+                hintNum[hint[stage][k] - 1]--;
             }
         }
     }
 
     public int solution(int[][] cost, int[][] hint) {
+
         Solution.cost = cost;
         Solution.hint = hint;
-        
-        answer = Integer.MAX_VALUE;
-        
-        memo = new HashMap<>();
 
-        dfs(0, 0, 0);
+        answer = Integer.MAX_VALUE;
+
+        dfs(0, 0, new int[cost.length]);
 
         return answer;
     }
